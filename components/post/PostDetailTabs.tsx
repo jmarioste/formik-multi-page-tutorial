fa54461e441a9b3f5fb7296a681566a3c004bee5
Jasser Mark Arioste <jassermark.arioste@gmail.com>
2022-11-20 22:05:17 +0800

    Add update button

--- components/post/PostDetailTabs.tsx
+++ components/post/PostDetailTabs.tsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import classNames from "classnames";
 import Link from "next/link";
 import { useRouter } from 'next/router';
+import PostContentTab from './PostContentTab';
+import PostInfoTab from './PostInfoTab';
+import PostSeoMetadataTab from './PostSeoMetadataTab';
+import UpdatePostButton from './UpdatePostButton';
 
 const PostDetailTabs = () => {
 
@@ -16,6 +20,15 @@ const PostDetailTabs = () => {
     { url: `/cms/post/${slug}/content`, text: "Content", activeMatcher: "content" },
     { url: `/cms/post/${slug}/seo`, text: "SEO Metadata", activeMatcher: "seo" }
   ]
+
+  const TabComponent = useMemo(() => {
+    switch (currentTab) {
+      case "content": return <PostContentTab />;
+      case "seo": return <PostSeoMetadataTab />;
+      default:
+        return <PostInfoTab />
+    }
+  }, [currentTab])
   return (
     <div>
       <div className="tabs">
@@ -30,6 +43,8 @@ const PostDetailTabs = () => {
           })
         }
       </div>
+      {TabComponent}
+      <UpdatePostButton />
     </div>
   )
 }
