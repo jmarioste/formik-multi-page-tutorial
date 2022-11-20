6f9e1f74a5d9608fea0fd501ff925ba6445dcc2f
Jasser Mark Arioste <jassermark.arioste@gmail.com>
2022-11-20 22:23:53 +0800

    Add navigation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,18 @@
 import type { NextPage } from "next";
+import Link from "next/link";
 
 const Home: NextPage = () => {
   //add state and a toggle handler
 
-  return <div>Home Page</div>
+  return <div className="container h-screen grid place-content-center">
+    <div className="flex flex-col items-center gap-4">
+      <h1 className="text-center text-xl">This is a DEMO on creating multi-page forms with Formik.</h1>
+      <p className="text-base-content/60">Click the button to go to the cms route</p>
+      <Link href={"/cms/post/hello-world"}>
+        <a className="btn btn-secondary">Go to Admin</a>
+      </Link>
+    </div>
+  </div>
 };
 
 export default Home;
